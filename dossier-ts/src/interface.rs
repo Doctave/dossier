eb1e7f0f6f7a99b5b92ac03d4e285c642dac8b75
Niklas Begley <niklas.begley@gmail.com>
2024-01-06 18:28:02 +0200

    Exporting and documenting interfaces

--- dossier-ts/src/interface.rs
+++ dossier-ts/src/interface.rs
@@ -1,4 +1,5 @@
 use crate::{
+    helpers::*,
     symbol::{Source, Symbol, SymbolKind},
     types, ParserContext,
 };
@@ -31,7 +32,9 @@ impl Interface {
 
     #[cfg(test)]
     pub fn properties(&self) -> impl Iterator<Item = &Symbol> {
-        self.children().iter().filter(|s| s.kind.as_property().is_some())
+        self.children()
+            .iter()
+            .filter(|s| s.kind.as_property().is_some())
     }
 }
 
@@ -59,10 +62,128 @@ pub(crate) fn parse(node: &Node, ctx: &mut ParserContext) -> Result<Symbol> {
         ctx,
         SymbolKind::Interface(Interface {
             identifier,
-            documentation: None,
+            documentation: find_docs(node, ctx.code).map(process_comment),
             object_type,
-            exported: false,
+            exported: is_exported(node),
         }),
         Source::for_node(node, ctx),
     ))
+}
+
+fn find_docs<'a>(node: &Node<'a>, code: &'a str) -> Option<&'a str> {
+    let parent = node.parent().unwrap();
+
+    if parent.kind() == "export_statement" {
+        if let Some(maybe_comment) = parent.prev_sibling() {
+            if maybe_comment.kind() == "comment" {
+                return Some(maybe_comment.utf8_text(code.as_bytes()).unwrap());
+            }
+        }
+    } else if let Some(maybe_comment) = node.prev_sibling() {
+        if maybe_comment.kind() == "comment" {
+            return Some(maybe_comment.utf8_text(code.as_bytes()).unwrap());
+        }
+    }
+
+    None
+}
+
+fn is_exported(node: &Node) -> bool {
+    if let Some(parent) = node.parent() {
+        if parent.kind() == "export_statement" {
+            return true;
+        }
+    }
+    false
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use dossier_core::tree_sitter::Parser;
+    use dossier_core::tree_sitter::TreeCursor;
+    use indoc::indoc;
+    use std::path::Path;
+
+    fn init_parser() -> Parser {
+        let mut parser = Parser::new();
+        parser
+            .set_language(tree_sitter_typescript::language_typescript())
+            .expect("Error loading TypeScript grammar");
+
+        parser
+    }
+
+    fn walk_tree_to_interface(cursor: &mut TreeCursor) {
+        assert_eq!(cursor.node().kind(), "program");
+        cursor.goto_first_child();
+        loop {
+            if cursor.node().kind() == "interface_declaration" {
+                break;
+            }
+            if cursor.node().kind() == "export_statement" {
+                cursor.goto_first_child();
+                cursor.goto_next_sibling();
+                break;
+            }
+
+            if !cursor.goto_next_sibling() {
+                panic!("Could not find interface_declaration node");
+            }
+        }
+    }
+
+    #[test]
+    fn documentation() {
+        let code = indoc! {r#"
+        /**
+         * This is a test interface.
+         */
+        interface Test {
+            test: string;
+        }
+        "#};
+
+        let tree = init_parser().parse(code, None).unwrap();
+        let mut cursor = tree.root_node().walk();
+        walk_tree_to_interface(&mut cursor);
+
+        let symbol = parse(
+            &cursor.node(),
+            &mut ParserContext::new(Path::new("index.ts"), code),
+        )
+        .unwrap();
+
+        assert_eq!(
+            symbol.kind.as_interface().unwrap().documentation,
+            Some("This is a test interface.".to_owned())
+        );
+    }
+
+    #[test]
+    fn exported() {
+        let code = indoc! {r#"
+        /**
+         * This is a test interface.
+         */
+        export interface Test {
+            test: string;
+        }
+        "#};
+
+        let tree = init_parser().parse(code, None).unwrap();
+        let mut cursor = tree.root_node().walk();
+        walk_tree_to_interface(&mut cursor);
+
+        let symbol = parse(
+            &cursor.node(),
+            &mut ParserContext::new(Path::new("index.ts"), code),
+        )
+        .unwrap();
+
+        assert!(
+            symbol.kind.as_interface().unwrap().exported,
+            "Should be exported"
+        );
+    }
 }
